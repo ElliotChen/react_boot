6943861dc6fdbd1add1092d5db9ced7ec6bf76b5
Elliot Chen <chenensign@gmail.com>
2017-05-02 17:29:05 +0800

    add node

---
+++ src/08/App.js
@@ -0,0 +1,9 @@
+/**
+ * Created by elliot on 02/05/2017.
+ */
+'use strict';
+
+const UtilFunction = require('./Utils.js');
+
+const uf = new UtilFunction();
+uf.sayHello();
---
+++ src/08/Utils.js
@@ -0,0 +1,16 @@
+/**
+ * Created by elliot on 02/05/2017.
+ */
+'use strict';
+
+class UtilFunction extends Object {
+	constructor() {
+		super();
+	}
+
+	sayHello() {
+		console.log("Hello!");
+	}
+}
+
+module.exports = UtilFunction;
